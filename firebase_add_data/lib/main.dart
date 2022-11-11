b8ff0326914ef6f9fe98c1ae9d3ab155cc170786
lllionlll <lam14.dev@gmail.com>
2022-11-11 14:55:56 +0700

    fix: add file into firebase -firebase storage

--- firebase_add_data/lib/main.dart
+++ firebase_add_data/lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,23 +34,63 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
+  List <String> list = [];
+  int _selectIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          child: Text('Open'),
-          onPressed: () async{
-            String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text('Open'),
+              onPressed: () async{
+                list.clear();
+                FilePickerResult? result = await FilePicker.platform.pickFiles(
+                  type: FileType.custom,
+                  allowedExtensions: ['jpg', 'pdf', 'doc'],
+                  allowMultiple: true
+                );
+                setState(() {
+                  result!.paths.forEach((element) {
+                    list.add(element.toString());
+                  });
+                });
+              },
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.green,
+              child: list.length > 0 ? Image.file(File(list[_selectIndex]), fit: BoxFit.cover,):null,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                UploadTask uploadTask;
+                final path = 'files';
+                final file = File(list.first);
+                final ref = FirebaseStorage.instance.ref().child(path);
+                uploadTask = ref.putFile(file);
 
-            if (selectedDirectory == null) {
-              // User canceled the picker
-            }
+                final snapshot = await uploadTask.whenComplete(() {});
 
-            print(selectedDirectory);
-          },
+                final urlDownload = await snapshot.ref.getDownloadURL();
+                print('Download Link: $urlDownload');
+              },
+              child: Text('Upload'),
+            )
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _selectIndex = _selectIndex == list.length - 1? 0: _selectIndex + 1;
+          });
+        },
+        child: Icon(Icons.arrow_forward_rounded, color: Colors.white,),
       ),
     );
   }
